6bb67470c389d170870f3b0531dbfecc7c33b9ad
Bridonneau Vincent <vincent.bridonneau@enseirb-matmeca.fr>
2019-12-12 13:46:10 +0100

    Translation done

--- includes/options.hpp
+++ includes/options.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include "transform.hpp"
 
 #include "cpptoml.h"
 
--- includes/transform.hpp
+++ includes/transform.hpp
@@ -2,6 +2,17 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <vtkXMLUnstructuredGridReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkDataSetMapper.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkXMLUnstructuredGridWriter.h>
 
 enum TransformType
 {
@@ -23,7 +34,9 @@ struct MergeParams : public Params
   bool                     m_compute_quality;
 
   MergeParams(std::vector<std::string> const& meshes, std::string const& result_file_name, bool compute_quality) :
-  kind(MergeTransform), m_meshes(meshes), m_result_file_name(result_file_name), m_compute_quality(compute_quality) {}
+  m_meshes(meshes), m_result_file_name(result_file_name), m_compute_quality(compute_quality) {
+    kind = MergeTransform;
+  }
 
   virtual ~MergeParams() = default;
   // TODO
@@ -36,7 +49,9 @@ struct TranslationParams : public Params
   bool                m_compute_quality;
 
   TranslationParams(std::vector<double> const& coords, std::string const& mesh, std::string const& result_file_name, bool compute_quality) :
-  kind(TranslationTransform), m_coords(coords), m_mesh(mesh), m_result_file_name(result_file_name), m_compute_quality(compute_quality) {}
+  m_coords(coords), m_mesh(mesh), m_result_file_name(result_file_name), m_compute_quality(compute_quality) {
+    kind = TranslationTransform;
+  }
 
   virtual ~TranslationParams() = default;
   // TODO
@@ -52,8 +67,37 @@ public:
 
   void operator()() {
     if (m_params -> kind == MergeTransform) {
-      std::shared_ptr<MergeTransform> merge_param(m_params);
+      std::shared_ptr<MergeParams> merge_param(dynamic_cast<MergeParams*>(m_params.get()));
       
+    } else if(m_params -> kind == TranslationTransform) {
+      std::shared_ptr<TranslationParams> translation_param(dynamic_cast<TranslationParams*>(m_params.get()));
+      std::string mesh(translation_param -> m_mesh);
+
+      /* Matrix Transform */
+
+
+      auto reader = vtkSmartPointer<vtkXMLUnstructuredGridReader>(vtkXMLUnstructuredGridReader::New());
+      reader -> SetFileName(mesh.c_str());
+      reader -> Update();
+
+      auto mesh_mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+      mesh_mapper -> SetInputConnection(reader -> GetOutputPort());
+
+      auto actor = vtkSmartPointer<vtkActor>(vtkActor::New());
+      actor -> SetMapper(mesh_mapper);
+
+      auto transform = vtkSmartPointer<vtkTransform>::New();
+      double *data = translation_param->m_coords.data();
+      transform -> Translate(translation_param->m_coords.data());
+
+      auto transformFilter = vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+      transformFilter -> SetInputConnection(mesh_mapper -> GetOutputPort());
+      transformFilter -> SetTransform(transform);
+
+      auto writer = vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
+      writer -> SetInputData(transformFilter -> GetOutput());
+      writer -> SetFileName(translation_param -> m_result_file_name . c_str());
+      writer -> Write();
     }
   }
 
